cf78dbecee8c9ddf9656558fcf5e33c783bbed41
iliverotti <iliverotti@ff2613c2-700e-11de-9895-81829a6523ab>
2009-07-15 03:45:08 +0000

    Se agregó un ejemplo de cómo hacer un efecto de fade in y fade out en MainScreenBehaviour.cs

--- Assets/DMScripts/MainScreenBehaviour.cs
+++ Assets/DMScripts/MainScreenBehaviour.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class MainScreenBehaviour : MonoBehaviour {
+	
+	Color backColor;
+	bool goingDown = true;
+	float fadeSpeed = 0.005f;
 
 	// Use this for initialization
 	void Start () {
-	
+		backColor = new Color(.5f,.5f,.5f,.5f);
 	}
 	
 	public void OnMouseDown(){
@@ -21,6 +25,36 @@ public class MainScreenBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(goingDown)
+		{
+			backColor.r -= fadeSpeed;
+			backColor.g -= fadeSpeed;
+			backColor.b -= fadeSpeed;
+			if(backColor.r < 0) {
+				backColor.r = backColor.g = backColor.b = 0.0f;
+				goingDown = false;
+			} // End if.
+		}
+		else
+		{
+			backColor.r += fadeSpeed;
+			backColor.g += fadeSpeed;
+			backColor.b += fadeSpeed;
+			if(backColor.r >= 0.5f) {
+				backColor.r = backColor.g = backColor.b = 0.5f;
+				goingDown = true;
+			} // End if.
+		} // End else.
+		
+		GameObject screenClick = GameObject.Find("001_Screen click_v2");
+		if(screenClick != null)
+		{
+			GUITexture screenBack = (GUITexture)screenClick.GetComponent("GUITexture");
+			if(screenBack != null)
+			{				
+				screenBack.color = backColor;
+			} // End if.
+		} // End if.
 	
 	}
 }
