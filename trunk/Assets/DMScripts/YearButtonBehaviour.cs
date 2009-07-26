4046438490a32c7ad638c9221773d9bddade8175
catalanojuan <catalanojuan@ff2613c2-700e-11de-9895-81829a6523ab>
2009-07-26 23:59:29 +0000

    Subo con la musica en la app principal.

--- trunk/Assets/DMScripts/YearButtonBehaviour.cs
+++ trunk/Assets/DMScripts/YearButtonBehaviour.cs
@@ -5,12 +5,20 @@ using System.Collections;
 public class YearButtonBehaviour : MonoBehaviour {
 
 	public int year;
+    public AudioSource screenAudio;
 	// Use this for initialization
 	void Start () {
-	
+        if (screenAudio != null)
+        {
+            screenAudio.Play();
+        }
 	}
 	
 	public void OnMouseDown(){
+        if (screenAudio != null)
+        {
+            screenAudio.Stop();
+        }
         Application.LoadLevel("ITBAFact" + year  + "Screen");
     }
 	
