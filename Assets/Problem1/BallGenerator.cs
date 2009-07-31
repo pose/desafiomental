1f88039592cd30e79d19b60e726638b7e5b17d3e
albertopose <albertopose@ff2613c2-700e-11de-9895-81829a6523ab>
2009-07-31 02:11:52 +0000

    Fondo que cambia de color en el juego 1.

--- Assets/Problem1/BallGenerator.cs
+++ Assets/Problem1/BallGenerator.cs
@@ -136,12 +136,43 @@ public class BallGenerator : MonoBehaviour
     
     }
 
-
+    Color color1 = new Color(0,0.6f,0,0);
+	Color color2 = new Color(0.6f,0,0,0);	
+	Color color3 = new Color(0,0,0.6f,0);
+	Color color4 = new Color(0.75f,0.75f,0.75f,0);
+	Color color5 = new Color(0.2f,0.2f,0.2f,0);
+	int pingPongStep = 0;
+	float duration = 15.0f;
+	
     private float timeMoved = 0.0f;
     private Vector3 direction = new Vector3(0.01f, 0);
     private Vector3 rotation = new Vector3(0.02f, 0);
     void Update()
     {
+        float t;
+        if (Time.time < duration)
+        {
+            t = Mathf.PingPong(Time.time, duration) / duration;
+            print(t);
+            Camera.main.backgroundColor = Color.Lerp(color1, color2, t);
+        }
+        else if (Time.time < 2 * duration)
+        {
+            t = Mathf.PingPong(Time.time - duration, duration) / duration;
+            Camera.main.backgroundColor = Color.Lerp(color2, color3, t);
+        }
+        else if (Time.time < 3 * duration)
+        {
+            t = Mathf.PingPong(Time.time - 2 * duration, duration) / duration;
+            Camera.main.backgroundColor = Color.Lerp(color3, color4, t);
+        }
+        else if (Time.time < 4 * duration)
+        {
+            t = Mathf.PingPong(Time.time - 3 * duration, duration) / duration;
+            Camera.main.backgroundColor = Color.Lerp(color4, color5, t);
+        }
+
+
         GameObject camera = GameObject.Find("DirectionalLightCamera");
         if (camera != null)
         {
