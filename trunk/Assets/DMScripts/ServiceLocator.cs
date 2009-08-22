35e55619afb3bbaf660693a1b6f0d404aa4a3f00
albertopose <albertopose@ff2613c2-700e-11de-9895-81829a6523ab>
2009-08-22 20:01:24 +0000

    mejoras a nivel código

---
+++ trunk/Assets/DMScripts/ServiceLocator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ServiceLocator : MonoBehaviour
+{
+    private static ServiceLocator instance = null;
+    private Hashtable h = new Hashtable();
+    void Start()
+    {   
+        DontDestroyOnLoad(this);
+        ServiceLocator.instance = this;
+    }
+
+    void Update()
+    {
+
+    }
+
+    public object Get(string s)
+    {
+        return h[s];
+    }
+
+    public void Set(string s, object o)
+    {
+        h[s] = o;
+    }
+
+    public static ServiceLocator GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = Instantiate(Resources.Load("ServiceLocator")) as ServiceLocator;
+        }
+        return instance;
+    }
+
+}
--- trunk/Assets/Problem8/LevelController/Scripts/LevelController.cs
+++ trunk/Assets/Problem8/LevelController/Scripts/LevelController.cs
@@ -2,36 +2,29 @@
 
 public class LevelController : MonoBehaviour
 {
-    public AudioSource song;
-    private GameObject[] targets;
+
+    public GameObject[] targets;
     private bool[] complete;
     public string onWinLoad = null;
+    public bool lastScene = false;
     private RaycastHit[] hits;
     private Link selectedLink = null;
     private PointsManagerBehaviour pmb;
     private MiniGamesGUI mg;
-    Color color1 = new Color(0.6f, 0.6f, 0.6f, 0);
-    Color color2 = new Color(0.1f, 0.1f, 0.1f, 0);
-    Color color3 = new Color(0.3f, 0.3f, 0.3f, 0);
-    Color color4 = new Color(0.75f, 0.75f, 0.75f, 0);
-    Color color5 = new Color(0.2f, 0.2f, 0.2f, 0);
-    int pingPongStep = 0;
-    float duration = 15.0f;
+
+
+
+    private ServiceLocator serviceLocator;
 
     void Start()
     {
+        //serviceLocator = ServiceLocator.GetInstance();
+
         GameObject go = GameObject.Find("GameManager");
-
-//        if ( song != null && !song.isPlaying)
-//            song.Play();
-        targets = new GameObject[transform.childCount];
-
-        int i = 0;
-        foreach (Transform g in transform)
+ 
+        for (int i = 0; i < targets.Length; i++)
         {
-            targets[i] = g.gameObject;
             targets[i].name = "Base" + i;
-            i++;
         }
 
         complete = new bool[targets.Length];
@@ -69,8 +62,6 @@ public class LevelController : MonoBehaviour
             if (complete[i] == false)
                 return;
         }
-        if ( song != null )
-            song.Stop();
 
         long totalPoints = 0;
         GameObject go = GameObject.Find("GameManager");
@@ -98,33 +89,16 @@ public class LevelController : MonoBehaviour
         } // End if.
 
 
-        if( onWinLoad != null && !onWinLoad.Equals("") )
+        if (onWinLoad != null && !onWinLoad.Equals(""))
+        {
+            if (onWinLoad.Equals("ResultsScreen")
             Application.LoadLevel(onWinLoad);
+
+        }
     }
 
     void Update()
     {
-        float t;
-        if (Time.time < duration)
-        {
-            t = Mathf.PingPong(Time.time, duration) / duration;
-            Camera.main.backgroundColor = Color.Lerp(color1, color2, t);
-        }
-        else if (Time.time < 2 * duration)
-        {
-            t = Mathf.PingPong(Time.time - duration, duration) / duration;
-            Camera.main.backgroundColor = Color.Lerp(color2, color3, t);
-        }
-        else if (Time.time < 3 * duration)
-        {
-            t = Mathf.PingPong(Time.time - 2 * duration, duration) / duration;
-            Camera.main.backgroundColor = Color.Lerp(color3, color4, t);
-        }
-        else if (Time.time < 4 * duration)
-        {
-            t = Mathf.PingPong(Time.time - 3 * duration, duration) / duration;
-            Camera.main.backgroundColor = Color.Lerp(color4, color5, t);
-        }
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         hits = Physics.RaycastAll(ray);
